104d3d9b21444b1dc20323390ee1c4cebb550219
Sandra2021211 <87116316+Sandra2021211@users.noreply.github.com>
2025-06-18 04:29:03 +0000

    Modified files

--- validator/driver.py
+++ validator/driver.py
@@ -1,7 +1,6 @@
 from file_read import FileReader, ColumnValidator
 from basic_properties import BasicPropertiesValidator
 from row_by_row_comparison import RowByRowComparator
-from streaming import StreamingData
 
 def main():
     # Define paths
@@ -9,11 +8,6 @@ def main():
     dest_path = "src_data/cleaned_insurance_claims.csv"
     map_path = "src_data/mapping.json"
 
-    #Sample test datasets and data frames to check for streaming.py
-    test_src="src_data/test_src_claims.csv"
-    test_dest="src_data/test_dest_claims.csv"
-    test_src_df = FileReader.read_csv(test_src)
-    test_dest_df = FileReader.read_csv(test_dest)
 
     # Read files
     source_df = FileReader.read_csv(src_path)
@@ -47,29 +41,10 @@ def main():
 
     #dest_df.loc[0, 'claim_payment_amount'] = 999999 - sample testcase for a mismatch
 
-    #comparison = RowByRowComparator(source_df,dest_df,col_mappings,src_primary_key,dest_primary_key)
-    #comparison.compare()
+    comparison = RowByRowComparator(source_df,dest_df,col_mappings,src_primary_key,dest_primary_key)
+    comparison.compare()
 
-    #To check mismatch in corrupted 1 week data
-
-    stream=StreamingData(test_src_df,test_dest_df)
-    stream.add_date(start_date='2025-04-01', end_date='2025-05-31')
-
-    #print("\n✅ StreamingData - Source with stream_time:")
-    #print(stream.src_df[['CLM_ID', 'stream_time']].head(1700))
-
-    #print("\n✅ StreamingData - Destination with stream_time:")
-    #print(stream.dest_df[['claim_id', 'stream_time']].head())
-
-    src_window,dest_window=stream.corrupted_dateframe(start='2025-04-15', end='2025-04-21')
     
-    src_window=src_window.drop_duplicates(subset='CLM_ID',keep='first')
-    dest_window=dest_window.drop_duplicates(subset='claim_id',keep='first')
-
-    print("\nComparing the 1 week corrupted window:")
-    window_comparator=RowByRowComparator(src_window,dest_window,col_mappings,src_primary_key,dest_primary_key)
-    window_comparator.compare()
-
 
 
 if __name__ == "__main__":
