a6c8e5e7dfff6d89b7bc5257016a8035e42f6ef1
Sandra2021211 <87116316+Sandra2021211@users.noreply.github.com>
2025-06-18 07:30:46 +0000

    Modified some changes

--- validator/stream.py
+++ validator/stream.py
@@ -12,15 +12,15 @@ class StreamingData:
         #self.src_df['stream_time'] = pd.to_datetime(self.src_df['stream_time'])
         #self.dest_df['stream_time'] = pd.to_datetime(self.dest_df['stream_time'])
 
-        print("✅ Source stream_time range:", self.src_df['stream_time'].min(), "to", self.src_df['stream_time'].max())
-        print("✅ Dest stream_time range:", self.dest_df['stream_time'].min(), "to", self.dest_df['stream_time'].max())
+        print("Source stream_time range:", self.src_df['stream_time'].min(), "to", self.src_df['stream_time'].max())
+        print("Dest stream_time range:", self.dest_df['stream_time'].min(), "to", self.dest_df['stream_time'].max())
 
 
         src_window = self.src_df[(self.src_df['stream_time'] >= c_start) & (self.src_df['stream_time'] <= c_end)]
         dest_window = self.dest_df[(self.dest_df['stream_time'] >= c_start) & (self.dest_df['stream_time'] <= c_end)]
 
-        print("🔸 Source window size:", len(src_window))
-        print("🔸 Destination window size:", len(dest_window))
+        print("Source window size:", len(src_window))
+        print("Destination window size:", len(dest_window))
 
 
         return src_window, dest_window
