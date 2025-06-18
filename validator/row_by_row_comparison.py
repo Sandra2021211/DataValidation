0121a1b888c563d8baa08dd9ecdfead986677100
Sandra2021211 <87116316+Sandra2021211@users.noreply.github.com>
2025-06-18 07:42:24 +0000

    Added time difference to check how long the code snippet runs

--- validator/row_by_row_comparison.py
+++ validator/row_by_row_comparison.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import time
 
 class RowByRowComparator:
     def __init__(self,source_df,dest_df,col_mappings,src_primary_key,dest_primary_key):
@@ -18,6 +19,8 @@ class RowByRowComparator:
 
     def compare(self):
         print("\nRow by Row comparison:")
+        start_time=time.time() #Start time
+
         mismatch=[]
 
         #Iterate through each row in source df
@@ -53,3 +56,7 @@ class RowByRowComparator:
                     print(f"Row {mis[0]} | Source: {mis[1]}-{mis[3]} , Destination: {mis[2]}-{mis[4]}")
         else:
             print("All rows match!!!")
+
+        end_time=time.time() #End time
+        time_diff=end_time-start_time
+        print(f"Time taken for row-by-row comparison: {time_diff:.2f} seconds")
